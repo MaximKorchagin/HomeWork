693193ea43bc8be6ce9417a7f9fe059128d6999d
MaximKorchagin <Airfallsc2@gmail.com>
2021-11-18 20:15:55 +0300

    first commit

---
+++ src/ru/netology/MaximKorchagin/Main.java
@@ -0,0 +1,19 @@
+package ru.netology.MaximKorchagin;
+
+import java.util.Random;
+
+public class Main {
+    public static final int SIZE = 8;
+
+    public static void main(String[] args) {
+        int[][] colors = new int[SIZE][SIZE];
+        int[][] reverseColors = new int[SIZE][SIZE];
+
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; i < SIZE; j++) {
+                
+            }
+        }
+    }
+}
